213e3680fc4f4e385b2e9152e5fb5aa96ebcfbf4
Harshit <harshitkishor2@gmail.com>
2020-09-17 18:36:08 +0530

    modified

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ require("./services/passport");
 require("./routes/authRoutes")(app);
 const port = process.env.PORT || 5000;
 
+app.get("/", (req, res) => {
+  res.send({
+    name: "harshit kishor",
+    email: "harshitkishor2@gmail.com",
+    age: 25,
+    hint: "for next page u can visit from this given link..",
+    link: "https://afternoon-thicket-97788.herokuapp.com/auth/google",
+  });
+});
 app.listen(port, () => {
   console.log(`Server started on ${port}`);
 });
